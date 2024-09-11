365c90837c9312beace1a4b4b069675e64bbb6e0
Laura Brekelmans <mail@laura.fm>
2024-09-11 13:06:04 +0200

    Bootstrap Ma runtime as MaObject

--- ma.ts
+++ ma.ts
@@ -17,13 +17,13 @@ type MaObjectMatcher = (it: MaObject) => boolean;
 type MaAction = (it: MaObject) => void;
 
 export class MaObject {
-  private claims: Map<any, any>;
+  protected claims: Map<any, any>;
   private wishHandlers: Map<string, {
     patternFn: WishMatcher,
     handler: WishHandler;
   }>;
 
-  constructor(public id: string, public  runtime: Ma) {
+  constructor(public id: string, private runtime: Ma|null) {
     this.claims = new Map();
     this.wishHandlers = new Map();
   }
@@ -46,7 +46,9 @@ export class MaObject {
   }
 
   wish(patternFn: MaObjectMatcher, wish: WishMessage) {
-    this.runtime.broadcastWish(this, patternFn, wish);
+    if (this.runtime) {
+      this.runtime.broadcastWish(this, patternFn, wish);
+    }
   }
 
   has(key: string) {
@@ -59,49 +61,51 @@ export class MaObject {
 }
 
 
-export class Ma {
-  private objects: Map<string, MaObject>;
-  private whens: {condition: MaObjectMatcher, action: MaAction}[];
-  private wishQueue: Wish[];
-
+export class Ma extends MaObject {
   constructor() {
-    this.objects = new Map();
-    this.whens = [];
-    this.wishQueue = [];
+    super("Ma", null);
+
+    this.claims.set("whens", []);
+    this.claims.set('wishQueue', []);
+    this.claims.set("objects", {
+      "Ma": this
+    });
   }
 
   createObject(program: (I: MaObject, ma: Ma) => void) {
     const id = genHexString(16);
     const obj = new MaObject(id, this);
-    this.objects.set(id, obj);
+    this.get('objects')[id] = obj;
     program(obj, this);
     return obj;
   }
 
   when(condition: MaObjectMatcher, action: MaAction) {
-    this.whens.push({ condition, action });
+    this.get('whens').push({ condition, action });
   }
 
   broadcastWish(sender: MaObject, patternFn: MaObjectMatcher, wish: WishMessage) {
-    this.wishQueue.push({sender, patternFn, wish});
+    this.get('wishQueue').push({sender, patternFn, wish});
   }
 
   evaluateWishes() {
-    while (this.wishQueue.length) {
-      const {sender, patternFn, wish} = this.wishQueue.pop() as Wish;
-      this.objects.forEach(obj => {
+    while (this.get('wishQueue').length) {
+      const {sender, patternFn, wish} = this.get('wishQueue').pop() as Wish;
+      const objects = Object.values(this.get('objects')) as MaObject[];
+      for (const obj of objects) {
         if (patternFn(obj)) {
           obj.receiveWish(sender, wish);
-          // In a real implementation, this would be more sophisticated
-          console.log(`Object ${obj.id} received wish: ${JSON.stringify(wish, null, 4)}`);
         }
-      });
+      }
     }
   }
 
   evaluateWhens() {
-    this.whens.forEach(({ condition, action }) => {
-      this.objects.forEach((object) => {
+    const whens = this.get('whens') as  {condition: MaObjectMatcher, action: MaAction}[];
+
+    whens.forEach(({ condition, action }) => {
+      const objects = Object.values(this.get('objects')) as MaObject[];
+      objects.forEach((object) => {
         if (condition(object)) {
           action(object);
         }
